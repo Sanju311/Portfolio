49cba33cd68087912f5644b942122a568fdb2e2c
Sanju Sathiyamoorthy <sanjumoorthy622@gmail.com>
2025-06-25 20:43:37 -0400

    Revert "base path change"
    This reverts commit 246fedcb1de318e0949e3e3a9bd4bf9e13ea11b2.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'Portfolio'; // Replace with your actual repository name
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true
   }
